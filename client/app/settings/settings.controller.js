bec20478de0459f25dcd2649d5b2eeacab084d0e
rajzz <raja.priyam777@gmail.com>
2016-04-04 23:45:22 +0530

    added profile

--- client/app/settings/settings.controller.js
+++ client/app/settings/settings.controller.js
@@ -1,70 +1,12 @@
 'use strict';
 
-angular.module('clickeatApp').controller('SettingsController', function($scope, Auth,$http){
-    $scope.errors = {};
-    $scope.submitted = false;
+angular.module('clickeatApp').controller('SettingsController', function($scope){
+  
+    $scope.step1 = true;
 
-    $scope.Auth = Auth;
-    $scope.changePassword = function(form) {
+    $scope.openTwo = function(i){
 
-      $scope.submitted = true;
-
-      if (form.$valid) {
-        $scope.Auth.changePassword($scope.user.oldPassword, $scope.user.newPassword).then(function () {
-          $scope.message = 'Password successfully changed.';
-        })['catch'](function () {
-          form.password.$setValidity('mongoose', false);
-          $scope.errors.other = 'Incorrect password';
-          $scope.message = '';
-        });
-      }
+      $scope['step'+i] = true;
     }
-
- var selected = null,
-        previous = null;
-    $scope.selectedIndex = 0;
-    $scope.$watch('selectedIndex', function(current, old){
-      previous = selected;
-    });
-    $scope.nextTab = function () {
-      if($scope.selectedIndex !== $scope.tabsLength){
-        $scope.selectedIndex = $scope.selectedIndex + 1;
-      } else{
-        
-      }
-    };
-    $scope.save = function () {
-      
-    };
-    $scope.varia = true;
-    $scope.tabsLength = 8;
-    $scope.user = Auth.getCurrentUser();
-    $scope.user.cateogry = "Student";
-
-
-
-      var readURL = function(input) {
-          if (input.files && input.files[0]) {
-              var reader = new FileReader();
-
-              reader.onload = function (e) {
-                  $('.profile-pic').attr('src', e.target.result);
-              }
-      
-              reader.readAsDataURL(input.files[0]);
-          }
-      }
-      
-
-      $scope.upload_change = function(){
-          readURL(this);
-      };
-      
-   /*   $(".upload-button").on('click', function() {
-      });*/
-      $scope.uploadClick = function(){
-       // angular.element(".file-upload").trigger("change");
-      };
-
 
 });
