8b5ebea5e5122e589aa6efc3f5a6264f7c8749e5
rajzz <raja.priyam777@gmail.com>
2016-04-13 09:21:02 +0530

    added profile editing for locum

--- client/app/settings/settings.controller.js
+++ client/app/settings/settings.controller.js
@@ -4,9 +4,55 @@ angular.module('clickeatApp').controller('SettingsController', function($scope){
   
     $scope.step1 = true;
 
+    $scope.user = {
+    	'referance':[
+    		{
+    			name:'referance1',
+    			mobile:9494979407,
+    			id:'refId1'
+    		},
+    		{
+    			name:'referance2',
+    			mobile:9494979408,
+    			id:'refId2'
+    		}
+    	],
+    	'nhssystem':[],
+    	'qualification':[],
+    	'languages':[]
+    };
+    $scope.qualifications = [ {id: 1, label: "MBBS"}, {id: 2, label: "BSC Nursing"}, {id: 3, label: "10"},{id:4,label:"10+2"}];
+    $scope.nhsdata = [ {id: 1, label: "NHS SYSTEM1"}, {id: 2, label: "NHS SYSTEM2"}, {id: 3, label: "NHS SYSTEM3"},{id:4,label:"NHS SYSTEM4"},{id:5,label:"NHS SYSTEM5"},{id:6,label:"NHS SYSTEM6"}];
+    $scope.languages = [ {id: 1, label: "Telugu"}, {id: 2, label: "French"}, {id: 3, label: "Jerman"}];
+    $scope.currentRef = {};
     $scope.openTwo = function(i){
 
       $scope['step'+i] = true;
     }
-
+    $scope.edit = function(index){
+    	$scope.currentRef = $scope.user.referance[index];
+    	$scope.currentIndex = index;
+    }
+    $scope.trash = function(index){
+    	$scope.user.referance.splice(index,1);
+    	if(index == $scope.currentIndex){
+    		$scope.reset();
+    	}
+    }
+    $scope.add = function(){
+    	if($scope.currentRef.name && $scope.currentRef.mobile &&$scope.currentRef.id){
+    		if($scope.currentIndex>=0){
+    			$scope.user.referance[$scope.currentIndex] = $scope.currentRef;
+    		}else{
+	    		$scope.user.referance.push($scope.currentRef);
+    		}
+	    	$scope.currentRef = {};
+    	}else{
+    		alert("id,mobile,name are mandetary fields");
+    	}
+    } 
+    $scope.reset = function(){
+    	$scope.currentIndex = undefined;
+    	$scope.currentRef = {};
+    }
 });
